ea95e3bb4957cf2542bf64f3dc6222405ac92bc3
Matthew Vanderhoff <m.vanderhoff94@gmail.com>
2014-11-20 18:56:42 -0400

    merged during rebase

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
 # Default is World
 # Author: Matthew Vanderhoff m.vanderhoff@mail.net
+require 'greeter'
+puts "enter you name"
 my_name = gets.strip
 
-puts "Hello, #{my_name}!"
+greeter = Greeter.new(my_name)
+puts greeter.greet
